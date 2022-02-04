20222255943b9e37ad5af6d55b896c980d8cf1e8
Woojung0618 <woojung0618@gmail.com>
2022-02-04 15:06:12 +0900

    Page, Route 초기설정

---
+++ src/components/CafeApplyForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CafeApplyForm = () => {
+
+    return (
+        <div>
+            CafeApplyForm
+        </div>
+    );
+};
+
+export default CafeApplyForm;
---
+++ src/pages/MainFarmer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MainFarmer = () => {
+    return (
+        <div>
+            MainFarmer
+        </div>
+    );
+};
+
+export default MainFarmer;
